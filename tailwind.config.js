407a66d0f992410b36f40bab5c62a3ba9c6b7bc1
Lakshmi manohar chippada <160750330+lchippada@users.noreply.github.com>
2025-02-09 17:13:28 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 1s ease-out',
+      },
+    },
+  },
+  plugins: [],
+};
